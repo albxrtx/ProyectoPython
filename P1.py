2385026382ed385dcccb481d916a471c695cee2d
albxrtx <albertomariannechita@gmail.com>
2024-10-24 20:50:02 +0200

    sidebar completo

--- P1.py
+++ P1.py
@@ -5,24 +5,27 @@ def imprimirDatos():
     print(f"Titulo: {cuadro_texto_1.get('1.0', tk.END).strip()}\nDescripcion: {cuadro_texto_2.get('1.0', tk.END).strip()}")
 
 ventana = tk.Tk()
-
+ventana.geometry("900x500")
 ventana.title("ToDo List")
-
+#Añadir icono .ico
 ventana.iconbitmap("")
 ventana.config(bg="lightgray")
-frame1 = ttk.Frame(width= 500,height=200)
-frame1.config()
-frame1.pack()
+
+frame1 = ttk.Frame(ventana)
+frame1.pack(fill=tk.Y, side=tk.LEFT)
+
+h1 = tk.Label(frame1,text="To Do List",font=("Helvetica", 30))
+h1.pack(pady=50)
 
 cuadro_texto_1 = tk.Text(frame1,width=30, height=2)
-cuadro_texto_1.pack(pady=10)
+cuadro_texto_1.pack(pady=10, padx=30)
 
 cuadro_texto_2 = tk.Text(frame1,width=30, height=2)
-cuadro_texto_2.pack(pady=10)
+cuadro_texto_2.pack(pady=10, padx=30)
 
 
-boton = tk.Button(frame1,text="Añadir",background="#36b167",foreground="#fefefe",padx = 30,pady = 10,font= 20,command= imprimirDatos)
-boton.pack()
+boton = tk.Button(frame1,text="Añadir",activebackground="#237544",activeforeground="#cecece", background="#36b167",border= 0,foreground="#fefefe",padx = 50,pady = 5,font=("Helvetica", 15),command= imprimirDatos)
+boton.pack(pady=10)
 
 
 # Siempre al final
