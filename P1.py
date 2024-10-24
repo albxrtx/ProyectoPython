25e8ef41bfe894612600396a04ace30287736d46
albxrtx <albertomariannechita@gmail.com>
2024-10-24 21:40:38 +0200

    solucionar llamada a la funcion

--- P1.py
+++ P1.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+import funciones as fn
 
-def imprimirDatos():
-    print(f"Titulo: {cuadro_texto_1.get('1.0', tk.END).strip()}\nDescripcion: {cuadro_texto_2.get('1.0', tk.END).strip()}")
 #Creamos ventana principal
 ventana = tk.Tk()
 ventana.geometry("1500x700")
@@ -29,8 +28,10 @@ descripcion_tarea_label.pack()
 cuadro_texto_2 = tk.Text(sidebar,width=30, height=2)
 cuadro_texto_2.pack(pady=10, padx=30)
 #Creamos un boton para añadir tareas
-boton = tk.Button(sidebar,text="Añadir",command= imprimirDatos,activebackground="#237544",activeforeground="#cecece", background="#36b167",border= 0,foreground="#fefefe",padx = 50,pady = 5,font=("Helvetica", 15))
-boton.pack(pady=10)
+msg1 = cuadro_texto_1.get('1.0', tk.END)
+msg2 = cuadro_texto_2.get('1.0', tk.END)
+boton = tk.Button(sidebar,text="Añadir",command=lambda: fn.obtenerTarea(msg1,msg2),activebackground="#237544",activeforeground="#cecece", background="#36b167",border= 0,foreground="#fefefe",padx = 50,pady = 5,font=("Helvetica", 15))
+boton.pack(pady=25)
 
 copy = tk.Label(sidebar,text= "©albxrtx Todos los derechos reservados")
 copy.pack(pady=10,side=tk.BOTTOM)
---
+++ funciones.py
@@ -0,0 +1,2 @@
+def obtenerTarea(msj1,msj2):
+    print(f"Nombre tarea: {msj1}Descripcion tarea: {msj2}")
