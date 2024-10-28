d340f7a880ec0557a40b67639665e501a87a594d
albxrtx <albertomariannechita@gmail.com>
2024-10-28 17:40:36 +0100

    tarea codificada

--- funciones.py
+++ funciones.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import pandas as pd
-
+import base64
 
 def obtenerTarea(nombre_tarea, descripcion_tarea, prioridad_tarea):
     nombre_tarea = nombre_tarea.strip()
@@ -8,7 +8,7 @@ def obtenerTarea(nombre_tarea, descripcion_tarea, prioridad_tarea):
     error = ""
 
     if nombre_tarea == "" or descripcion_tarea == "":
-        error = "No puedo haber un cuadro vacio"
+        error = "Ambos campos deben estar completos"
         crear_ventana_error(error)
     elif prioridad_tarea == "Prioridad":
         error = "Selecciona un prioridad"
@@ -34,9 +34,21 @@ def crear_ventana_error(error):
 
 
 def introducir_nueva_tarea(nombre, descripcion, prioridad):
+    #Codificamos los datos base64
+    nombre_codificado = base64.b64encode(bytes(nombre,'utf-8'))
+    descripcion_codificado = base64.b64encode(bytes(descripcion,'utf-8'))
+    prioridad_codificado = base64.b64encode(bytes(prioridad,'utf-8'))
+    # Decodificamos los datos
+    # prioridad_decodificado = base64.b64decode(prioridad_codificado).decode('utf-8')
+
+    #Leemos el csv 
     archivo_tareas = pd.read_csv("tareas.csv")
-    nueva_fila = {"nombre": nombre, "descripcion": descripcion, "prioridad": prioridad}
+    #Creamos una nueva fila con los datos introducidos por el usuario
+    nueva_fila = {"nombre": nombre_codificado, "descripcion": descripcion_codificado, "prioridad": prioridad_codificado}
+    #Creamos un DataFrame con la nueva fila
     new_df = pd.DataFrame([nueva_fila])
+    #Añadimos el DataFrame al csv
     archivo_tareas = pd.concat([archivo_tareas, new_df])
     archivo_tareas.to_csv("tareas.csv", index=False)
+    #Mensaje de confirmación
     print("Tarea añadida correctamente")
